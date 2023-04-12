66e1f5af431e6466cb2f8c882967258927264e75
omarlawah <omarxxxxx135@gmail.com>
2023-04-12 16:47:04 +0000

    ...

--- scripts.js
+++ scripts.js
@@ -1,44 +1,36 @@
-let grid = document.querySelector('.pad');
-function createGrid(){ 
-for (let i=0;i<16*16;i++){
-    const cell = document.createElement('div');
-    grid.appendChild(cell);
-    cell.addEventListener('mouseover', function(){
-        cell.style.backgroundColor='black';
-    })
-}};
-
-const choose = document.querySelector('.choose');
-choose.addEventListener('click', createGrid); 
+let gridSize=0;
+let container = document.querySelector('.pad');
+container.style.display = "grid";
 
 
+function updateSize(val) {
+    container.innerHTML=''; 
+    gridSize= document.getElementById('resize').textContent = val;     
+    container.style.gridTemplateColumns = `repeat(${gridSize},  minmax(0, 1fr))`;
+    container.style.gridTemplateRows = `repeat(${gridSize},  minmax(0, 1fr))`;  
+   drawGrid(val);
+   return gridSize;
+   }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+function drawGrid(){
+    for(let i=0;i<gridSize * gridSize;i++){
+        const div = document.createElement('div');
+        container.appendChild(div);
+        div.classList.add('box');
+        div.addEventListener('mouseover', function(){
+            div.style.backgroundColor ='black';
+        });
+    };    
+}
+ 
 
 
 
-
-
-
-const reset = document.querySelector('.reset');
-reset.addEventListener('click', ()=>{
-    document.location.reload();
-}); 
-
-
-
+//let buttons= document.querySelectorAll('button');
+/*button.forEach(buttons,()=>{
+    
+})*/
 
 
 
